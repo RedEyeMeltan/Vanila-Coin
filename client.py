f07088595f40f8d53fee48451b6009b9443aea7b
CookieClickerdev <168486721+CookieClickerdev@users.noreply.github.com>
2024-10-21 22:29:46 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+import wmi
+
+w = wmi.WMI() # Initialize WMI object
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "172.24.96.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048))
+
+send("hi")
+send(DISCONNECT_MESSAGE)
