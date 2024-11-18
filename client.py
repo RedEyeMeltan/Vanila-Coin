50331b2dd3067da8f6a0318d0e4941973b84b966
RedEyeMeltan <105523764+RedEyeMeltan@users.noreply.github.com>
2024-11-17 21:05:48 -0800

    Update client.py

--- client.py
+++ client.py
@@ -8,8 +8,9 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "172.21.160.1"
+SERVER = "192.168.56.1"
 ADDR = (SERVER, PORT)
+ID_CODE = ":$z35$7LW$"
 
 #  THE HASH IS TESTED WITH "test"
 verifyhash = 'test'
@@ -17,9 +18,21 @@ single_test = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
 double_test = "7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c"
 triple_test = "5b24f7aa99f1e1da5698a4f91ae0f4b45651a1b625c61ed669dd25ff5b937972"
 
+cpu_id = w.Win32_Processor()[0].ProcessorId
+disk_serial = w.Win32_DiskDrive()[0].SerialNumber
+ram = w.Win32_PhysicalMemory()[0]
+ram_id = ram.SerialNumber
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
+def hwidSend():
+    print("Sending hardware info...")
+    send(f"{ID_CODE}CPU ID: {cpu_id}")
+    send(f"{ID_CODE}Disk Serial Number: {disk_serial}")
+    send(f"{ID_CODE}RAM ID: {ram_id}")
+    print("Hardware info sent successfully.")
+
 
 def singleHash(content):
     global single_hex_dig
@@ -70,9 +83,12 @@ def send(msg):
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
-    print(client.recv(2048))
+    response = client.recv(2048).decode(FORMAT)  # Decode response
+    print(f"{response}")
 
 
 
-send("hi")
+
+hwidSend()
+print("Sending disconnect message...")
 send(DISCONNECT_MESSAGE)
